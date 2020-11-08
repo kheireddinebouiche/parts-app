31539e1b9a65d3c72108c78a5dbf073bd9640c88
kheireddinebouiche <40995455+kheireddinebouiche@users.noreply.github.com>
2020-11-08 20:30:03 +0100

    Mise forme de la page contact

--- parts/contact/forms.py
+++ parts/contact/forms.py
@@ -2,9 +2,10 @@ from django import forms
 
 
 class ContactForm(forms.Form):
+    nom = forms.CharField(required=True, label='Votre nom', widget=forms.TextInput(attrs={'type':'text','class':'form-control'}))
     from_email = forms.EmailField(required=True, label='Votre adresse Email',widget=forms.EmailInput(attrs={'type':'email','class':'form-control'}))
     subject = forms.CharField(required=True, label='Le suject de votre message',widget=forms.TextInput(attrs={'type':'text','class':'form-control'}))
     message = forms.CharField(required=True, label='Votre message', widget=forms.Textarea(attrs={'type':'text','class':'form-control'}))
-    nom = forms.CharField(required=True, label='Votre nom', widget=forms.TextInput(attrs={'type':'text','class':'form-control'}))
+    
     
 
